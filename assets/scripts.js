d8390cdd2482bd1db2a8dcb7b4095c5645d3f794
Benoit <147654072+SinicX3@users.noreply.github.com>
2025-01-08 13:47:52 +0100

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,3 +13,29 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
+
+function () {
+    const target = document.GetElementById("#contact");
+    const div = assignObject(createElement("div"), {className: "quote"});
+    const div_title = assignObject(createElement("div"));
+    const div_ul = assignObject(createElement("div"));
+
+    div.innerText("Bonjour");
+  
+    target.insertAdjacentElement("afterend", div);
+    
+}
+
+	<div class="quote">
+		<div>
+			<h2>Mes coordonnées</h2>
+		</div>
+		<div>
+			<ul itemscope itemtype="https://schema.org/LocalBusiness">
+				<li><span itemprop="name">Nina Carducci</span></li>
+				<li>Adresse : <span itemprop="address">68 avenue Alsace-Lorraine, 33200 Bordeaux</span>.</li>
+				<li>Téléphone : <span itemprop="telephone">05 56 67 78 89</span>.</li>
+				<li>Je réponds au téléphone <span itemprop="openingHours" datetime="Monday through Friday, 10:00-19:00">du lundi au vendredi de 10 h à 19 h</span>.</li>
+			</ul>
+		</div>
+	</div>
