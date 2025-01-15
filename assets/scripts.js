75f96bebf9d95e94c4047957b3b4ea024a30e1c5
Benoit Thery <thery.benoit@hotmail.fr>
2025-01-15 12:43:15 +0100

    Coordonnees generees par script

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,28 +13,3 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
-
-function Coords () {
-
-    const target = document.getElementById("contact");
-    const div = Object.assign(document.createElement("div"), {className:"quote"});
-    const div_title = Object.assign(document.createElement("div"), {innerHTML:"<h2>Mes coordonnées</h2>"});
-    const div_ul = Object.assign(document.createElement("div"));
-
-    div.appendChild(div_title);
-  
-    const ul = Object.assign(document.createElement("ul"), {itemtype:"https://schema.org/LocalBusiness"});
-    ul.setAttribute("itemscope", "");
-    const li1 = Object.assign(document.createElement("li"), {innerHTML:"<span itemprop=`name`>Nina Carducci</span>"});
-    const li2 = Object.assign(document.createElement("li"), {innerHTML:"Adresse : <span itemprop=`address`>68 avenue Alsace-Lorraine, 33200 Bordeaux</span>."});
-    const li3 = Object.assign(document.createElement("li"), {innerHTML:"Téléphone : <span itemprop=`telephone`>05 56 67 78 89</span>."});
-    const li4 = Object.assign(document.createElement("li"), {innerHTML:"Je réponds au téléphone <span itemprop=`openingHours` datetime=`Monday through Friday, 10:00-19:00`>du lundi au vendredi de 10 h à 19 h</span>."});
-
-    [li1, li2, li3, li4].forEach((e) => ul.appendChild(e));
-    div_ul.appendChild(ul);
-    div.appendChild(div_ul);
-    target.insertAdjacentElement("afterend", div);
-    
-}
-
-//Coords();
